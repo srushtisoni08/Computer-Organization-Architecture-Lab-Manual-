b23ea4602d7a276f15d0ce7f8e471095c7adc050
srushti soni <164400584+srushtisoni08@users.noreply.github.com>
2025-01-21 17:07:03 +0530

    Create decimal_to_binary.c

---
+++ practical 1/decimal_to_binary.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int decimal=0,i=0;
+    int binary=0;
+    int num[50];
+    printf("Enter your decimal number:");
+    scanf("%d",&decimal);
+    while(decimal>0){
+        num[i] = decimal%2;
+        decimal = decimal/2;
+        i++;
+    }
+    for(int j=i-1;j>=0;j--){
+        binary = binary*10 + num[j];
+    }
+    printf("%d",binary);
+}
